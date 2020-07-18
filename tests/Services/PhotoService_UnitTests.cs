fbac2bece08de219648e412fa0f159a01200be66
Anthony <anthony.r.larson@gmail.com>
2020-07-17 22:30:49 -0500

    Additional setup

---
+++ tests/Services/PhotoService_UnitTests.cs
@@ -0,0 +1,22 @@
+using Xunit;
+
+public class PhotoService_UnitTests
+{
+    [Fact]
+    public void GetAlbumById_ValidAlbumIdPassed_ReturnsAlbum()
+    {
+
+    }
+
+    [Fact]
+    public void GetAlbumById_NonNumericIdPassed_ThrowsInvalidDataException()
+    {
+
+    }
+
+    [Fact]
+    public void GetAlbumById_NullValuePassed_ThrowsArgumentNullException()
+    {
+
+    }
+}
